b0b2a1d9515502991975812c0aa5ce9c8467e21a
anaismiraira <mirairaanais@gmail.com>
2022-03-11 19:21:30 -0400

    1921 11032022 project and contact

--- assets/index.js
+++ assets/index.js
@@ -71,3 +71,62 @@ const galleryTop = new Swiper('.gallery-top', {
     swiper: galleryThumbs
   }
 });
+// ------------------------------Gallery-Advance-----------------------------
+$("#lightgallery .swiper-wrapper").lightGallery();
+//---------------------------------Form-------------------------------
+function dataSubmited(data) {
+  const requestOptions = {
+    method: 'POST',
+    body: data,
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+  };
+  fetch("https://www.infocasas.com.uy/proyectos/austin?&formulario=1&json=1", requestOptions)
+  .then((json) => {
+    setTimeout(()=>{
+      if (json.status === 200) {
+        $('#formSuccess').fadeIn();
+      } else {
+        $('#formError').fadeIn();
+      }
+      $('#formSending').hide();
+    }, 2000)
+  })
+  .catch(error => {
+    console.log('error', error);
+    setTimeout(() => {
+      $('#formSending').hide();
+      $('#formError').fadeIn();
+    }, 2000)
+  });
+}
+
+function submited() {
+ 'use strict'
+  const form = document.querySelector('#contactForm')
+  const data = JSON.stringify({
+    nombre: form.name.value,
+    apellido: "",
+    email: form.email.value,
+    telefono: form.phone.value,
+    tel: form.phone.value,
+    source: 2,
+    utm_source: "web_cliente",
+    utm_medium: "austin",
+    extra: form.consult.value,
+    InfoLeads: 1,
+    IDflow_execution: 4315
+  })
+  if (!form.checkValidity()) {
+    event.preventDefault()
+    event.stopPropagation()
+  }else{
+    dataSubmited(data)
+    setTimeout(()=>{
+      $(form).fadeOut();
+      $('#formSending').fadeIn();
+    },300)
+  }
+  form.classList.add('was-validated')
+}
